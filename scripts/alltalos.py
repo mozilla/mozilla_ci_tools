d8e6b385fb5c08e26595865040536806edeb6feb
Joel Maher <joel.maher@gmail.com>
2015-03-06 12:15:45 -0500

    addressed feedback, updated for bitrot

--- scripts/alltalos.py
+++ scripts/alltalos.py
@@ -8,7 +8,7 @@ from mozci.mozci import trigger_job, query_builders
 
 logging.basicConfig(format='%(asctime)s %(levelname)s:\t %(message)s',
                     datefmt='%m/%d/%Y %I:%M:%S')
-log = logging.getLogger()
+LOG = logging.getLogger()
 
 PGO_ONLY_BRANCHES = ['mozilla-aurora', 'mozilla-beta']
 
@@ -16,25 +16,33 @@ PGO_ONLY_BRANCHES = ['mozilla-aurora', 'mozilla-beta']
 def talos_buildernames(repo_name, pgo_only=False):
     """
         This function aims to generate all possible talos jobs for a given branch.
-        #TODO: what if jobs change (new jobs/platforms that come with an uplift or get removed)?
         #TODO: somehow mozilla beta/aurora has non-pgo buildernames when we only do pgo
     """
     builders = query_builders()
     buildernames = []
 
+    # Android and OSX do not have PGO, so we need to get those specific jobs
     pgo_re = re.compile(".*%s pgo talos.*" % repo_name)
-    osx_re = re.compile(".*OSX.*%s talos.*" % repo_name)
-    android_re = re.compile(".*Android.* %s talos.*" % repo_name)
     talos_re = re.compile(".*%s talos.*" % repo_name)
 
-    regex_list = [talos_re]
-    if repo_name in PGO_ONLY_BRANCHES or pgo_only:
-        regex_list = [pgo_re, osx_re, android_re]
-
+    talos_jobs = set()
+    pgo_jobs = set()
+    tp_jobs = set()
     for builder in builders:
-        for regex in regex_list:
-            if regex.match(builder):
-                buildernames.append(builder)
+        if talos_re.match(builder):
+            talos_jobs.add(builder)
+
+        if pgo_re.match(builder):
+            pgo_jobs.add(builder)
+            tp_jobs.add(builder.replace(' pgo', ''))
+
+    if repo_name in PGO_ONLY_BRANCHES or pgo_only:
+        non_pgo_jobs = talos_jobs - tp_jobs
+        talos_jobs = pgo_jobs.union(non_pgo_jobs)
+
+    buildernames = []
+    for builder in talos_jobs:
+        buildernames.append(builder)
 
     buildernames.sort()
     return buildernames
@@ -71,6 +79,11 @@ def parse_args(argv=None):
                         dest="debug",
                         help="set debug for logging.")
 
+    parser.add_argument("--pgo",
+                        action="store_true",
+                        dest="pgo",
+                        help="trigger pgo tests (not non-pgo).")
+
     options = parser.parse_args(argv)
     return options
 
@@ -79,14 +92,13 @@ def main():
     options = parse_args()
 
     if options.debug:
-        log.setLevel(logging.DEBUG)
-        log.info("Setting DEBUG level")
+        LOG.setLevel(logging.DEBUG)
+        LOG.info("Setting DEBUG level")
 
-    buildernames = talos_buildernames(options.repo_name)
+    buildernames = talos_buildernames(options.repo_name, options.pgo)
 
     for buildername in buildernames:
-        trigger_job(repo_name=options.repo_name,
-                    revision=options.revision,
+        trigger_job(revision=options.revision,
                     buildername=buildername,
                     times=options.times,
                     dry_run=options.dry_run)
