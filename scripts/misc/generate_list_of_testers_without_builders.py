2a8289a156538a1ea18aa53971578ab9c99213f6
Armen Zambrano G <armenzg@mozilla.com>
2015-03-11 14:19:34 -0400

    Misc script to generate list of testers without builders

---
+++ scripts/misc/generate_list_of_testers_without_builders.py
@@ -0,0 +1,28 @@
+'''
+This script generates a list of buildbot test builders that do not
+have a build to trigger it.
+'''
+import logging
+
+from mozci.platforms import determine_upstream_builder
+from mozci.mozci import query_builders
+
+
+logging.basicConfig(format='%(asctime)s %(levelname)s:\t %(message)s',
+                    datefmt='%m/%d/%Y %I:%M:%S')
+LOG = logging.getLogger()
+
+
+def main():
+    list = []
+
+    for builder in query_builders():
+        if determine_upstream_builder(builder) is None:
+            list.append(builder)
+
+    for x in sorted(list):
+        print x
+
+
+if __name__ == '__main__':
+    main()
