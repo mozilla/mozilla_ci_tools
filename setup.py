b752d918589eb5d342e476d05c3395c33e7bc55e
Alice Scarpa <alicescarpa@gmail.com>
2015-07-21 18:03:00 -0300

    Preparing release 0.11.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mozci',
-    version='0.10.2.dev0',
+    version='0.11.0',
     packages=find_packages(),
     entry_points ={
         'console_scripts': [
