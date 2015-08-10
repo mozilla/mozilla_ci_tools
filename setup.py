eea6c2a6e9606549f3faead3661e08b83d95dd0d
Vaibhav Agrawal <vaibhavmagarwal@gmail.com>
2015-08-10 14:49:42 -0700

    Preparing release 0.13.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mozci',
-    version='0.12.2.dev0',
+    version='0.13.0',
     packages=find_packages(),
     entry_points ={
         'console_scripts': [
