392a8b0b43d9f406f81d1c3d2cb9df2c1b4d40e5
Vaibhav Agrawal <vaibhavmagarwal@gmail.com>
2015-08-13 15:27:35 -0700

    Back to development: 0.13.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mozci',
-    version='0.13.2',
+    version='0.13.3.dev0',
     packages=find_packages(),
     entry_points ={
         'console_scripts': [
