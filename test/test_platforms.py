mozilla/mozilla_ci_tools

commit e4b5d8233e0e3ba3afe12c3761a7656221e6afd1

Major refactoring of platforms.py to improve future development.
mozci.py
--------
* query_builders() can now handle getting builders of one repo
* trigger_missing_jobs_for_revisions() now uses list_builders() to
access builders of a repo without using string matching

platforms.py
------------
* We now discard certain builders with _wanted_builder()
* get_buildername_metadata() gives a curated set of important
* metadata about
a builder
* _get_test() renamed to _get_suite_name()
* build_tests_per_platform_graph() refactored for readability &
extensibility
* _include_builders_matching() & _exclude_builders_matching()
* instead of
_filter_builders_matching()
* refactored find_buildernames for readability

test_platforms.py
-----------------
* Changes due to platforms.py function renames
* Clean up of code
* Remove unused patch calls

buildbot_brdige.py
------------------
* Make use of enhanced get_buildername_metadata()

mozci/mozci.py
@@ -15,9 +15,14 @@ from __future__ import absolute_import
 import logging
 
 from mozci.errors import MozciError
-from mozci.platforms import determine_upstream_builder, is_downstream, \
-    filter_buildernames, build_talos_buildernames_for_repo
-from mozci.sources import allthethings, buildapi, buildjson, pushlog
+from mozci.platforms import (
+    build_talos_buildernames_for_repo,
+    determine_upstream_builder,
+    filter_buildernames,
+    is_downstream,
+    list_builders,
+)
+from mozci.sources import buildapi, buildjson, pushlog
 from mozci.query_jobs import (
     PENDING,
     RUNNING,
@@ -261,9 +266,9 @@ def _find_files(job_schedule_info):
 #
 # Query functionality
 #
-def query_builders():
-    """Return list of all builders."""
-    return allthethings.list_builders()
+def query_builders(repo_name=None):
+    """Return list of all builders or the builders associated to a repo."""
+    return list_builders(repo_name)
 
 
 def query_repo_name_from_buildername(buildername, clobber=False):
@@ -490,11 +495,11 @@ def trigger(builder, revision, files=[], dry_run=False, extra_properties=None):
 def trigger_missing_jobs_for_revision(repo_name, revision, dry_run=False):
     """
     Trigger missing jobs for a given revision.
-    Jobs have any of ('hg bundle', 'b2g', 'pgo') in their buildername will not be triggered.
+    Jobs containing 'b2g' or 'pgo' in their buildername will not be triggered.
     """
-    all_buildernames = filter_buildernames([repo_name],
-                                           ['hg bundle', 'b2g', 'pgo'],
-                                           allthethings.list_builders())
+    all_buildernames = filter_buildernames(
+        exclude=['b2g', 'pgo'],
+        builders=list_builders(repo_name=repo_name))
 
     for buildername in all_buildernames:
         trigger_range(buildername=buildername,

commit 21591a35bf84bc2f4b741ce9949b2e6f6c753ba9

Bug 1214625 - Ignore non pgo talos builders + fix test regressions
When trying to fill in a revision we need to determine the list of
available builders for that repo.

In the case of mozilla-aurora and mozilla-beta, allthethings.json
contains both the pgo and non-pgo builders (Buildbot bug).

There is no parent builder for mozci to trigger and we fail.

This change ignores these builders.

This change also refactors the trigger.py script and adds a
--fill-revision option to test this change.

Read below for changes fixing regressions introduced in
e4b5d8233e0e3ba3afe12c3761a7656221e6afd1

mozci.py
---
* For trigger_missing_jobs_for_revision() do not use
filter_buildernames() (string matching) but list_builders().
It fixes including invalid talos builders

platforms.py
---
* Add job_type and suite_name to get_buildername_metadata()
* Drop _get_suite_name()
* In build_tests_per_platform_graph() fix regression and use
list_builders() instead of _include_builders_matching() (string
matching).
* Change signature of filter_buildernames()
* Improve _wanted_builder() logic to differentiate talos builders on
mozilla-{aurora,beta} properly
* Fix regression in list_builders()

test_platforms.py
---
* Clean up + minor refactor
* Drop test_get_suite_name() for TestGetBuildernameMetadata

mozci/mozci.py
@@ -18,7 +18,6 @@ from mozci.errors import MozciError
 from mozci.platforms import (
     build_talos_buildernames_for_repo,
     determine_upstream_builder,
-    filter_buildernames,
     is_downstream,
     list_builders,
 )
@@ -497,18 +496,20 @@ def trigger_missing_jobs_for_revision(repo_name, revision, dry_run=False):
     Trigger missing jobs for a given revision.
     Jobs containing 'b2g' or 'pgo' in their buildername will not be triggered.
     """
-    all_buildernames = filter_buildernames(
-        exclude=['b2g', 'pgo'],
-        builders=list_builders(repo_name=repo_name))
+    builders_for_repo = list_builders(repo_name=repo_name)
 
-    for buildername in all_buildernames:
-        trigger_range(buildername=buildername,
-                      revisions=[revision],
-                      times=1,
-                      dry_run=dry_run,
-                      extra_properties={'mozci_request': {
-                                        'type': 'trigger_missing_jobs_for_revision'}
-                                        })
+    for buildername in builders_for_repo:
+        trigger_range(
+            buildername=buildername,
+            revisions=[revision],
+            times=1,
+            dry_run=dry_run,
+            extra_properties={
+                'mozci_request': {
+                    'type': 'trigger_missing_jobs_for_revision'
+                }
+            }
+        )
 
 
 def trigger_all_talos_jobs(repo_name, revision, times, dry_run=False):

mozci/platforms.py
@@ -68,7 +68,7 @@ def _process_data():
         if not _wanted_builder(buildername):
             continue
 
-        if not is_downstream(buildername):
+        if is_upstream(buildername):
             SHORTNAME_TO_NAME[builderinfo['shortname']] = buildername
             BUILD_JOBS[buildername.lower()] = buildername
 
@@ -163,22 +163,36 @@ def get_buildername_metadata(buildername):
     """Return metadata associated to a buildername.
 
     Returns a dictionary with the following information:
-        * build_type - It returns 'opt', 'debug' or 'pgo'
+        * build_type - It returns 'opt' or 'debug'
         * downstream - If the job requires an upstream job to be triggered
-        * platform_name - It associates upstream and downstream builders (e.g. win32)
+        * job_type - It returns 'build', 'test' or 'talos'
+        * platform_name - It associates upstream & downstream builders (e.g. win32)
         * product - e.g. firefox
         * repo_name - Associated short name for a repository (e.g. alder)
+        * suite_name - talos & test jobs have an associated suite name (e.g chromez)
     """
-    builder_info = _get_raw_builder_metadata(buildername)
-    props = builder_info['properties']
+    if buildername not in fetch_allthethings_data()['builders']:
+        return None
+
+    props = _get_raw_builder_metadata(buildername)['properties']
 
     # For talos tests we have to check stage_platform
     if 'talos' in buildername:
         platform_name = props['stage_platform']
-    else:
+        job_type = 'talos'
+        # 'Windows 7 32-bit mozilla-central pgo talos chromez-e10s' -> chromez-e10s
+        suite_name = buildername.split(" ")[-1]
+    elif 'test' in buildername:
+        job_type = 'test'
         platform_name = props['platform']
+        suite_name = buildername.split(" ")[-1]
+    else:
+        job_type = 'build'
+        platform_name = props['platform']
+        suite_name = None
 
     if platform_name.endswith('-debug'):
+        # e.g. win32-debug
         platform_name = platform_name[:-len('-debug')]
         build_type = 'debug'
     else:
@@ -194,33 +208,24 @@ def get_buildername_metadata(buildername):
     else:
         downstream = 'slavebuilddir' in props and props['slavebuilddir'] == 'test'
 
-    assert platform_name is not None
     assert build_type is not None
     assert repo_name is not None
     assert repo_path is not None
+    assert platform_name is not None
 
     return {
         'build_type': build_type,
         'downstream': downstream,
+        'job_type': job_type,
         'platform_name': platform_name,
         'product': props['product'],
-        'repo_name': repo_name
+        'repo_name': repo_name,
+        'suite_name': suite_name,
     }
 
 
 def get_associated_platform_name(buildername):
     return get_buildername_metadata(buildername)['platform_name']
-
-
-def _get_suite_name(buildername):
-    """
-    For test jobs, the test type is the last part of the name.
-
-    For example:
-    in Windows 7 32-bit mozilla-central pgo talos chromez-e10s
-    the suite name is chromez-e10s
-    """
-    return buildername.split(" ")[-1]
 
 
 def _get_job_type(test_job):
@@ -250,6 +255,7 @@ def build_tests_per_platform_graph(builders):
 
     # Let's separate upstream from downstream builders
     for builder in builders:
+        # Ignore invalid builders
         if not _wanted_builder(builder):
             continue
 
@@ -281,7 +287,8 @@ def build_tests_per_platform_graph(builders):
         build_type = info['build_type']
         platform_name = info['platform_name']
         # Suite name from the downstream builder
-        suite_name = _get_suite_name(downstream_builder)
+        dn_builder_info = get_buildername_metadata(downstream_builder)
+        suite_name = dn_builder_info['suite_name']
 
         # Some builders in allthethings (for example, "Ubuntu Code
         # Coverage VM 12.04 x64 try debug test cppunit") are not
@@ -376,16 +383,13 @@ def find_buildernames(repo, suite_name=None, platform=None, job_type='opt'):
     assert suite_name is not None or platform is not None, \
         'suite_name and platform cannot both be None.'
 
-    buildernames = _include_builders_matching(
-        builders=list_builders(),
-        keyword=' %s ' % repo
-    )
+    buildernames = list_builders(repo)
 
     if suite_name is not None:
-        buildernames = _include_builders_matching(
-            builders=buildernames,
-            keyword=suite_name
-        )
+        buildernames = filter(
+            lambda x:
+            get_buildername_metadata(x)['suite_name'] == suite_name,
+            buildernames)
     # If no specific suite has been chosen we should then select all tests jobs
     else:
         buildernames = filter(lambda x: is_downstream(x), buildernames)
@@ -401,7 +405,7 @@ def find_buildernames(repo, suite_name=None, platform=None, job_type='opt'):
     return buildernames
 
 
-def filter_buildernames(include, exclude, buildernames):
+def filter_buildernames(buildernames, include=[], exclude=[]):
     """Return every builder matching the words in include and not in exclude."""
 
     for word in include:
@@ -414,20 +418,39 @@ def filter_buildernames(include, exclude, buildernames):
 
 
 def _wanted_builder(builder, filter=True, repo_name=None):
+    """ Filter unnecessary builders that Buildbot's setup has. """
     if filter:
         # Builders to ignore
         if builder.startswith('release-') or \
            builder.endswith('bundle'):
             return False
 
+    # We lack metadata in allthethings for release builders
+    # in order to call get_buildername_metadata()
     info = get_buildername_metadata(builder)
+
     if repo_name and repo_name != info['repo_name']:
         return False
+
+    # Exclude the non pgo talos builders for m-a and m-b
+    # *if* there is no pgo talos builder
+    # This is to work around bug 1149514
+    if filter:
+        if info['repo_name'] in ('mozilla-aurora', 'mozilla-beta') and \
+           info['job_type'] == 'talos' and _get_job_type(builder) == 'opt':
+
+            equiv_pgo_builder = builder.replace('talos', 'pgo talos')
+            if equiv_pgo_builder in fetch_allthethings_data()['builders']:
+                # There are two talos builders, we only can use the pgo one
+                return False
+            else:
+                # This platform only has only one talos builder
+                return True
 
     return True
 
 
-def list_builders(filter=True, repo_name=None):
+def list_builders(repo_name=None, filter=True):
     """Return a list of all builders running in the buildbot CI."""
     all_builders = fetch_allthethings_data()['builders']
     assert len(all_builders) > 0, "The list of builders cannot be empty."
@@ -435,7 +458,7 @@ def list_builders(filter=True, repo_name=None):
     # Let's filter out builders which are not triggered per push
     # and are not associated to a repo_name if set
     builders_list = []
-    for builder in all_builders:
+    for builder in all_builders.keys():
         if _wanted_builder(builder=builder, filter=filter, repo_name=repo_name):
             builders_list.append(builder)
 

mozci/scripts/trigger.py
@@ -10,7 +10,8 @@ from mozci.mozci import (
     query_repo_name_from_buildername,
     query_repo_url_from_buildername,
     set_query_source,
-    trigger_range
+    trigger_missing_jobs_for_revision,
+    trigger_range,
 )
 from mozci.query_jobs import BuildApi, COALESCED
 from mozci.sources.buildapi import (
@@ -88,7 +89,29 @@ def parse_args(argv=None):
                         help="Set files (typically an installer and test zip url "
                         "to be used in triggered jobs.")
 
-    # These are the various modes in which we can run this script
+    parser.add_argument("--repo-name",
+                        dest="repo_name",
+                        help="Branch name")
+
+    parser.add_argument("--existing-only",
+                        action="store_false",
+                        dest="trigger_build_if_missing",
+                        help="Only trigger test jobs if the build jobs already exists.")
+
+    # Mode #1: Coalesced jobs of a revision
+    parser.add_argument("--coalesced",
+                        action="store_true",
+                        dest="coalesced",
+                        help="Trigger every coalesced job on revision --rev "
+                        "and repo --repo-name.")
+
+    # Mode #2: Add all missing jobs for a revision
+    parser.add_argument("--fill-revision",
+                        action="store_true",
+                        dest="fill_revision",
+                        help="Add all missing jobs to a revision.")
+
+    # Mode #3: Trigger jobs and 3 modifiers of the list of revisions to trigger on
     parser.add_argument("--delta",
                         dest="delta",
                         type=int,
@@ -105,21 +128,6 @@ def parse_args(argv=None):
                         help="We will trigger jobs starting from --rev in reverse chronological "
                         "order until we find the last revision where there was a good job.")
 
-    parser.add_argument("--coalesced",
-                        action="store_true",
-                        dest="coalesced",
-                        help="Trigger every coalesced job on revision --rev "
-                        "and repo --repo-name.")
-
-    parser.add_argument("--existing-only",
-                        action="store_false",
-                        dest="trigger_build_if_missing",
-                        help="Only trigger test jobs if the build jobs already exists.")
-
-    parser.add_argument("--repo-name",
-                        dest="repo_name",
-                        help="Branch name")
-
     options = parser.parse_args(argv)
     return options
 
@@ -129,9 +137,10 @@ def validate_options(options):
     Raises an exception if options are missing or conflicting.
     """
     error_message = ""
-    if not options.buildernames and not options.coalesced:
-        error_message = "A buildername is mandatory for all modes except --coalesced. " \
-                        "Use --buildername."
+    if not options.buildernames and (not options.coalesced and not options.fill_revision):
+        error_message = "A buildername is mandatory for all modes except --coalesced and " \
+                        "--fill-revision. Use --buildername."
+
     if options.coalesced and not options.repo_name:
         error_message = "A branch name is mandatory with --coalesced. Use --repo-name."
 
@@ -139,7 +148,6 @@ def validate_options(options):
         if options.backfill or options.delta or options.from_rev:
             error_message = "You should not pass --backfill, --delta or --end-rev " \
                             "when you use --back-revisions."
-
     elif options.backfill:
         if options.delta or options.from_rev:
             error_message = "You should not pass --delta or --end-rev " \
@@ -211,6 +219,7 @@ def determine_revlist(repo_url, buildername, rev, back_revisions,
 def main():
     options = parse_args()
     validate_options(options)
+
     if not valid_credentials():
         sys.exit(-1)
 
@@ -234,6 +243,7 @@ def main():
         options.rev = query_repo_tip(repo_url)
         LOG.info("The tip of %s is %s", options.repo_name, options.rev)
 
+    # Mode 1: Trigger coalesced jobs
     if options.coalesced:
         query_api = BuildApi()
         request_ids = query_api.find_all_jobs_by_status(options.repo_name,
@@ -247,6 +257,16 @@ def main():
 
         return
 
+    # Mode #2: Fill-in a revision
+    if options.fill_revision:
+        trigger_missing_jobs_for_revision(
+            repo_name=options.repo_name,
+            revision=options.rev,
+            dry_run=options.dry_run
+        )
+        return
+
+    # Mode #3: Trigger jobs based on revision list modifiers
     for buildername in options.buildernames:
         revlist = determine_revlist(
             repo_url=repo_url,

test/test_platforms.py
@@ -8,16 +8,17 @@ from mock import patch
 
 from mozci.platforms import (
     _get_job_type,
-    _get_suite_name,
     _include_builders_matching,
     build_tests_per_platform_graph,
     build_talos_buildernames_for_repo,
     determine_upstream_builder,
+    get_associated_platform_name,
+    get_buildername_metadata,
     get_downstream_jobs,
     filter_buildernames,
     find_buildernames,
-    get_associated_platform_name,
     is_downstream,
+    list_builders,
 )
 
 
@@ -82,8 +83,9 @@ class TestFindBuildernames(unittest.TestCase):
                 repo='mozilla-beta',
                 suite_name='tp5o',
                 job_type=None)),
+            # 'Platform1 mozilla-beta talos tp5o' is not considered a valid
+            # builder and we won't expect it
             ['Platform1 mozilla-beta pgo talos tp5o',
-             'Platform1 mozilla-beta talos tp5o',
              'Platform2 mozilla-beta talos tp5o'])
 
     @patch('mozci.platforms.fetch_allthethings_data')
@@ -142,7 +144,8 @@ class TestGetPlatform(unittest.TestCase):
         """For build jobs it should return the platform attribute."""
         fetch_allthethings_data.return_value = MOCK_ALLTHETHINGS
         self.assertEquals(
-            get_associated_platform_name('Platform1 repo build'), 'platform1')
+            get_associated_platform_name('Platform1 repo build'),
+            'platform1')
 
 
 class TestBuildGraph(unittest.TestCase):
@@ -153,21 +156,25 @@ class TestBuildGraph(unittest.TestCase):
     def test_build_graph(self, fetch_allthethings_data):
         """Test if the graph has the correct format."""
         fetch_allthethings_data.return_value = MOCK_ALLTHETHINGS
-        builders = _include_builders_matching(
-            MOCK_ALLTHETHINGS['builders'].keys(),
-            ' repo '
-        )
+        builders = list_builders(repo_name='repo')
         builders.sort()
         expected = {
-            'debug': {'platform1':
-                      {'tests': ['mochitest-1'],
-                       'Platform1 repo leak test build':
-                       ['Platform1 repo debug test mochitest-1']}},
-            'opt': {'platform1':
-                    {'tests': ['mochitest-1', 'tp5o'],
-                     'Platform1 repo build':
-                     ['Platform1 repo opt test mochitest-1',
-                      'Platform1 repo talos tp5o']}}}
+            'debug': {
+                'platform1': {
+                    'tests': ['mochitest-1'],
+                    'Platform1 repo leak test build': [
+                        'Platform1 repo debug test mochitest-1']
+                }
+            },
+            'opt': {
+                'platform1': {
+                    'tests': ['mochitest-1', 'tp5o'],
+                    'Platform1 repo build': [
+                        'Platform1 repo opt test mochitest-1',
+                        'Platform1 repo talos tp5o']
+                }
+            }
+        }
 
         self.assertEquals(build_tests_per_platform_graph(builders), expected)
 
@@ -197,12 +204,13 @@ class TestDetermineUpstream(unittest.TestCase):
 
     @patch('mozci.platforms.fetch_allthethings_data')
     def test_invalid(self, fetch_allthethings_data):
-        """The function should raise an Exception buildernames not in allthethings.json."""
+        """Raises Exception for buildernames not in allthethings.json."""
         fetch_allthethings_data.return_value = MOCK_ALLTHETHINGS
         with pytest.raises(Exception):
             determine_upstream_builder("Not a valid buildername")
-        # Since "Platform1 mozilla-beta pgo talos tp5o" exists, "Platform1 mozilla-beta talos tp5o"
-        # is an invalid buildername and should return None
+        # Since "Platform1 mozilla-beta pgo talos tp5o" exists,
+        # "Platform1 mozilla-beta talos tp5o" is an invalid buildername
+        # and should return None
         self.assertEquals(
             determine_upstream_builder("Platform1 mozilla-beta talos tp5o"), None)
 
@@ -216,10 +224,11 @@ class TestGetDownstream(unittest.TestCase):
         """Test if the function finds the right downstream jobs."""
         fetch_allthethings_data.return_value = MOCK_ALLTHETHINGS
         self.assertEquals(
-            get_downstream_jobs('Platform1 repo build'), sorted([
+            sorted(get_downstream_jobs('Platform1 repo build')),
+            [
                 'Platform1 repo opt test mochitest-1',
                 'Platform1 repo talos tp5o'
-            ]))
+            ])
 
 
 class TestTalosBuildernames(unittest.TestCase):
@@ -244,17 +253,19 @@ class TestTalosBuildernames(unittest.TestCase):
         self.assertEquals(build_talos_buildernames_for_repo('not-a-repo'), [])
 
 
-get_test_test_cases = [
-    ("Windows 8 64-bit mozilla-aurora pgo talos dromaeojs", "dromaeojs"),
-    ("Android 2.3 Emulator mozilla-release opt test plain-reftest-7", "plain-reftest-7")]
+class TestGetBuildernameMetadata(unittest.TestCase):
+    """ Test that we can evaluate metadata correctly """
 
-
-@pytest.mark.parametrize("test, expected", get_test_test_cases)
-def test_get_suite_name(test, expected):
-    """Test _get_suite_name with test cases from get_test_test_cases."""
-    obtained = _get_suite_name(test)
-    assert obtained == expected, \
-        'obtained: "%s", expected "%s"' % (obtained, expected)
+    @patch('mozci.platforms.fetch_allthethings_data')
+    def test_suite_name(self, fetch_allthethings_data):
+        """Test if the function finds the right suite_name."""
+        fetch_allthethings_data.return_value = MOCK_ALLTHETHINGS
+        test_cases = [
+            ("Platform1 repo talos tp5o", "tp5o"),
+            ("Platform1 repo opt test mochitest-1", "mochitest-1")
+        ]
+        for t in test_cases:
+            self.assertEquals(get_buildername_metadata(t[0])['suite_name'], t[1])
 
 
 def test_include_builders_matching():