1bdebf0c0432de7828bd38f807f7e1430713adb1
Armen Zambrano G <armenzg@mozilla.com>
2015-10-23 17:59:36 -0400

    Back to development: 0.17.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mozci',
-    version='0.17.0',
+    version='0.17.1.dev0',
     packages=find_packages(),
     entry_points ={
         'console_scripts': [
