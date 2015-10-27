dba84e7c7b2229b5a1d5deed7ae15931a727936f
Armen Zambrano G <armenzg@mozilla.com>
2015-10-27 13:42:35 -0400

    Preparing release 0.18.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mozci',
-    version='0.17.1.dev0',
+    version='0.18.0',
     packages=find_packages(),
     entry_points ={
         'console_scripts': [
