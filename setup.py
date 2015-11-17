fa8089448ae5228a355a0115feb1360601b9c64b
Armen Zambrano G <armenzg@mozilla.com>
2015-11-17 10:00:17 -0500

    Preparing release 0.21.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mozci',
-    version='0.20.1.dev0',
+    version='0.21.0',
     packages=find_packages(),
     entry_points ={
         'console_scripts': [
