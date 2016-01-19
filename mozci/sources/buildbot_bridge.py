mozilla/mozilla_ci_tools

commit b43a3c19fb751d5826586539ac438afe1112c998

Bug 1240156 - pulse_actions should associated a request to the requester
Pulse_actions does not associate a push extender request to the requester but to the cached information of a
previous push.

This refactoring permits the tool calling the TaskCluster scheduler to
specify the metadata associated to the task graph scheduled.

mozci/sources/buildbot_bridge.py
@@ -39,6 +39,7 @@ from mozci.sources.tc import (
     get_task_graph_status,
     create_task,
     generate_task_graph,
+    generate_metadata,
     schedule_graph,
     extend_task_graph,
 )
@@ -48,7 +49,7 @@ LOG = logging.getLogger('mozci')
 
 
 def _create_task(buildername, repo_name, revision, task_graph_id=None,
-                 parent_task_id=None, requires=None, properties={}):
+                 parent_task_id=None, requires=None, properties={}, *args, **kwargs):
     """Return takcluster task to trigger a buildbot builder.
 
     This function creates a generic task with the minimum amount of
@@ -107,7 +108,11 @@ def _create_task(buildername, repo_name, revision, task_graph_id=None,
             },
             'properties': all_properties,
         },
-        metadata_name=buildername
+        metadata=generate_metadata(
+            repo_name=repo_name,
+            revision=revision,
+            name=buildername,
+        )
     )
 
     if requires:
@@ -206,8 +211,6 @@ def generate_tc_graph_from_builders(builders, repo_name, revision):
 
     """
     return generate_task_graph(
-        repo_name=repo_name,
-        revision=revision,
         scopes=[
             # This is needed to define tasks which take advantage of the BBB
             'queue:define-task:buildbot-bridge/buildbot-bridge',
@@ -216,6 +219,11 @@ def generate_tc_graph_from_builders(builders, repo_name, revision):
             builders=builders,
             repo_name=repo_name,
             revision=revision
+        ),
+        metadata=generate_metadata(
+            repo_name=repo_name,
+            revision=revision,
+            name='Mozci BBB graph'
         )
     )
 
@@ -345,7 +353,7 @@ def generate_graph_from_builders(repo_name, revision, buildernames, *args, **kwa
         builders_graph=buildbot_graph_builder(buildernames, revision)[0])
 
 
-def generate_builders_tc_graph(repo_name, revision, builders_graph):
+def generate_builders_tc_graph(repo_name, revision, builders_graph, *args, **kwargs):
     """Return TaskCluster graph based on builders_graph.
 
     NOTE: We currently only support depending on one single parent.
@@ -378,7 +386,9 @@ def generate_builders_tc_graph(repo_name, revision, builders_graph):
             repo_name=repo_name,
             revision=revision,
             builders_graph=builders_graph
-        )
+        ),
+        *args,
+        **kwargs
     )
 
     return task_graph

mozci/sources/tc.py
@@ -13,13 +13,11 @@ import traceback
 import taskcluster as taskcluster_client
 from taskcluster.utils import slugId, fromNow
 
-from mozci.errors import TaskClusterError
 from mozci.repositories import query_repo_url
 from mozci.sources.pushlog import query_revision_info
 
 
 LOG = logging.getLogger('mozci')
-METADATA = None
 TC_TOOLS_HOST = 'https://tools.taskcluster.net'
 TC_TASK_INSPECTOR = "%s/task-inspector/#" % TC_TOOLS_HOST
 TC_TASK_GRAPH_INSPECTOR = "%s/task-graph-inspector/#" % TC_TOOLS_HOST
@@ -54,26 +52,29 @@ def handle_auth_failure(e):
                   "TASKCLUSTER_ACCESS_TOKEN")
 
 
-def _query_metadata(repo_name, revision, name, description=None):
-    global METADATA
+def generate_metadata(repo_name, revision, name,
+                      description='Task graph generated via Mozilla CI tools'):
+    """ Generate metadata based on input
 
-    if not METADATA:
-        repo_url = query_repo_url(repo_name)
-        push_info = query_revision_info(repo_url, revision)
+    :param repo_name: e.g. alder, mozilla-central
+    :type repo_name: str
+    :param revision: 12-chars representing a push
+    :type revision: str
+    :param name: Human readable name of task-graph, give people finding this an idea
+                 what this graph is about.
+    :type name: str
+    :param description: Human readable description of task-graph, explain what it does!
+    :type description: str
+    """
+    repo_url = query_repo_url(repo_name)
+    push_info = query_revision_info(repo_url, revision)
 
-        if not description:
-            description = 'Task graph generated via Mozilla CI tools'
-
-        METADATA = {
-            'description': description,
-            'owner': push_info['user'],
-            'source': '%s/rev/%s' % (repo_url, revision),
-        }
-
-    result = {'name': name}
-    result.update(METADATA)
-
-    return result
+    return {
+        'name': name,
+        'description': description,
+        'owner': push_info['user'],
+        'source': '%s/rev/%s' % (repo_url, revision),
+    }
 
 
 def get_task(task_id):
@@ -94,18 +95,12 @@ def get_task_graph_status(task_graph_id):
     return response['status']['state']
 
 
-def create_task(repo_name, revision, **kwargs):
+def create_task(**kwargs):
     """ Create a TC task.
 
     NOTE: This code needs to be tested for normal TC tasks to determine
     if the default values would also work for non BBB tasks.
     """
-    metadata = _query_metadata(
-        repo_name,
-        revision,
-        name=kwargs.get('metadata_name')
-    )
-
     task_id = kwargs.get('taskId', slugId())
 
     task_definition = {
@@ -119,7 +114,7 @@ def create_task(repo_name, revision, **kwargs):
             'deadline': kwargs.get('deadline', fromNow('1d')),
             'expires': kwargs.get('deadline', fromNow('1d')),
             'payload': kwargs.get('payload', {}),
-            'metadata': kwargs.get('metadata', metadata),
+            'metadata': kwargs['metadata'],  # mandatory
             'schedulerId': kwargs.get('schedulerId', 'task-graph-scheduler'),
             'tags': kwargs.get('tags', {}),
             'extra': kwargs.get('extra', {}),
@@ -215,7 +210,7 @@ def retrigger_task(task_id, dry_run=False):
     return results
 
 
-def schedule_graph(task_graph, task_graph_id=None, dry_run=False):
+def schedule_graph(task_graph, task_graph_id=None, dry_run=False, *args, **kwargs):
     """ It schedules a TaskCluster graph and returns its id.
 
     :param task_graph: It is a TaskCluster graph as defined in here:
@@ -276,20 +271,13 @@ def extend_task_graph(task_graph_id, task_graph, dry_run=False):
         return scheduler.extendTaskGraph(task_graph_id, task_graph)
 
 
-def generate_task_graph(scopes, tasks, repo_name=None, revision=None,
-                        metadata=None):
+def generate_task_graph(scopes, tasks, metadata, *args, **kwargs):
+    """ It creates a TC task graph ready to be scheduled.
+    """
     if 'scheduler:create-task-graph' not in scopes:
         scopes.append('scheduler:create-task-graph')
 
-    if not metadata:
-        if repo_name and revision:
-            metadata = _query_metadata(repo_name, revision, 'task graph local')
-        else:
-            raise TaskClusterError(
-                "You have to either specify repo_name/revision or metadata"
-            )
-
-    # XXX: we could validation in here
+    # XXX: We could do validation in here
     task_graph = {
         'scopes': scopes,
         'tasks': tasks,

mozci/ci_manager.py
@@ -6,6 +6,7 @@ Defined in here:
     * BaseCIManager
     * BuildAPIManager
     * TaskclusterManager
+    * TaskClusterBuildbotManager
 """
 from __future__ import absolute_import
 
@@ -164,6 +165,7 @@ class TaskClusterBuildbotManager(TaskClusterManager):
             repo_name=repo_name,
             revision=revision,
             builders_graph=builders_graph,
+            metadata=kwargs.get('metadata'),
         )
         return super(TaskClusterBuildbotManager, self).schedule_graph(
             task_graph=task_graph, *args, **kwargs)

commit b8495367c3f49556f3e983d43465e55d4e837d3e

Fix regression introduced by b43a3c19fb751d5826586539ac438afe1112c998

mozci/sources/buildbot_bridge.py
@@ -376,8 +376,6 @@ def generate_builders_tc_graph(repo_name, revision, builders_graph, *args, **kwa
 
     # This is the initial task graph which we're defining
     task_graph = generate_task_graph(
-        repo_name=repo_name,
-        revision=revision,
         scopes=[
             # This is needed to define tasks which take advantage of the BBB
             'queue:define-task:buildbot-bridge/buildbot-bridge',
@@ -387,8 +385,11 @@ def generate_builders_tc_graph(repo_name, revision, builders_graph, *args, **kwa
             revision=revision,
             builders_graph=builders_graph
         ),
-        *args,
-        **kwargs
+        metadata=generate_metadata(
+            repo_name=repo_name,
+            revision=revision,
+            name='Mozci BBB graph'
+        )
     )
 
     return task_graph
@@ -489,8 +490,6 @@ def trigger_builders_based_on_task_id(repo_name, revision, task_id, builders,
         required_task_ids = []
 
     task_graph = generate_task_graph(
-        repo_name=repo_name,
-        revision=revision,
         scopes=[
             # This is needed to define tasks which take advantage of the BBB
             'queue:define-task:buildbot-bridge/buildbot-bridge',
@@ -503,6 +502,11 @@ def trigger_builders_based_on_task_id(repo_name, revision, task_id, builders,
             parent_task_id=task_id,
             # This creates dependencies on other tasks
             required_task_ids=required_task_ids,
+        ),
+        metadata=generate_metadata(
+            repo_name=repo_name,
+            revision=revision,
+            name='Mozci BBB graph'
         )
     )
 

mozci/sources/tc.py
@@ -271,7 +271,7 @@ def extend_task_graph(task_graph_id, task_graph, dry_run=False):
         return scheduler.extendTaskGraph(task_graph_id, task_graph)
 
 
-def generate_task_graph(scopes, tasks, metadata, *args, **kwargs):
+def generate_task_graph(scopes, tasks, metadata):
     """ It creates a TC task graph ready to be scheduled.
     """
     if 'scheduler:create-task-graph' not in scopes: