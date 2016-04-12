30e1f979f44df774d80017244a64f066712deb56
Armen Zambrano <armenzg@users.noreply.github.com>
2016-04-12 09:02:38 -0400

    Trying to fix package (#457)

--- scripts/setup.py
+++ scripts/setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mozci_scripts',
-    version='0.1.0',
+    name='mozci-scripts',
+    version='0.1.2',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
