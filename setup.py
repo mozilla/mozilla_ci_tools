fe0086d26976a96146c7e3aca89f344378ae62bc
Armen Zambrano G <armenzg@mozilla.com>
2016-07-15 12:40:52 -0400

    Back to development: 0.43.2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ required = [
 
 setup(
     name='mozci',
-    version='0.43.1',
+    version='0.43.2.dev0',
     packages=find_packages(),
     install_requires=required + ['pytest-runner'],
     tests_require=required + ['mock', 'pytest'],
