c48f6a0b17ee2e99bba83b8d52acdc64ec11da8b
Armen Zambrano G <armenzg@mozilla.com>
2016-09-02 13:13:44 -0400

    Preparing release 0.44.2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ required = [
 
 setup(
     name='mozci',
-    version='0.44.2.dev0',
+    version='0.44.2',
     packages=find_packages(),
     install_requires=required + ['pytest-runner'],
     tests_require=required + ['mock', 'pytest'],
