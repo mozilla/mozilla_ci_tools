5e992c58ce5d43870bae88dfdfce2a53dc6fbddf
Armen Zambrano G <armenzg@gmail.com>
2016-11-29 13:48:41 -0500

    Preparing release 0.51.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ required = [
 
 setup(
     name='mozci',
-    version='0.50.1.dev0',
+    version='0.51.0',
     packages=find_packages(),
     install_requires=required + ['pytest-runner'],
     tests_require=required + ['mock', 'pytest'],
